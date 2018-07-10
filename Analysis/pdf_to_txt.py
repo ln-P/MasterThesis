4242a02c7ed0bd12c1701baaadcafe411a5ced29
Wiktor Owczarz <wiktor.owczarz@gmail.com>
2018-07-10 22:47:26 +0200

    Crening up code and creating function that can be reused in the future

--- Analysis/pdf_to_txt.py
+++ Analysis/pdf_to_txt.py
@@ -3,7 +3,6 @@ This script loads collected PDFs and saves them in .txt format
 
 Created on: Do 21. Jun 15:06:21 CEST 2018
 """
-import json
 import os
 import sys
 from timeit import default_timer as timer
@@ -18,42 +17,54 @@ else:
     authors_path = "/Users/Wiktor/Dropbox/Git/MasterThesis/Literature/Competition_Studies_Database/authors.json"
     studies_location = "/Users/Wiktor/Dropbox/Git/MasterThesis/Literature/Competition_Studies_Database/"
 
-# Reading studies database
-with open(os.path.join(studies_path)) as file:
-    studies = json.load(file)
 
-# Load studies labels
-study_labels = [x for x in os.listdir(studies_location)]
+def read_and_transform(studies_path):
+    """
+    Function to read all .pdf files from given location and transform them into .txt files
 
-# Excluding problematic texts this lead to removing study 17
-# exclude = ['p017.pdf']
-# study_labels = [text for text in study_labels if text not in exclude]
+    Argumens:
+     - studies_path: path to .json file, database with collected competition studies
+     - studies_location: location of the directory where the collected documents are located
 
-study_paths = [studies_location + x for x in study_labels]
+    Returns:
+     - .txt files in given directory that correspond to the matching .pdfs
+    """
 
-# Excluding not .pdf files
-study_labels = [pdf for pdf in study_labels if '.pdf' in pdf]
-study_paths = [pdf for pdf in study_paths if '.pdf' in pdf]
+    # Load studies labels
+    study_labels = [x for x in os.listdir(studies_location)]
+
+    # Creating paths to the documents
+    study_paths = [studies_location + x for x in study_labels]
+
+    # Excluding not .pdf files
+    study_labels = [pdf for pdf in study_labels if '.pdf' in pdf]
+    study_paths = [pdf for pdf in study_paths if '.pdf' in pdf]
+
+    # Creating list of doubles study label and its path
+    reading_list = zip(study_labels, study_paths)
+
+    # Start time
+    start = timer()
+
+    # Loading each study and converting it to txt file
+    for label, text in reading_list:
+
+        print("Loading paper: {}".format(label))
+
+        # Creating .txt file with label as a name
+        write_file = label.split('.')[0] + '.txt'
+
+        with open(write_file, 'w+') as text_file:
+            # Writing converted pdf to .txt format
+            text_file.write(pdf2text(text))
+            text_file.close()
+
+        print("Study saved to: {}".format(write_file))
+
+    end = timer()
+
+    print("Process took: {} seconds".format(end - start))
 
 
-reading_list = list(zip(study_labels, study_paths))
-
-start = timer()
-
-for label, text in zip(study_labels, study_paths):
-
-    print("Loading paper: {}".format(label))
-
-    # Creating .txt file with label as a name
-    write_file = label.split('.')[0] + '.txt'
-
-    with open(write_file, 'w+') as text_file:
-        # Writing converted pdf to .txt format
-        text_file.write(pdf2text(text))
-        text_file.close()
-
-    print("Study saved to: {}".format(write_file))
-
-end = timer()
-
-print("Process took: {} seconds".format(end - start))
+if __name__ == '__main__':
+    read_and_transform(studies_path=studies_path)
