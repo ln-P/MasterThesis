eb2a776d3c4e68a0ca03a4c5d1801d48edf3fd9a
Wiktor Owczarz <wiktor.owczarz@gmail.com>
2018-08-25 22:32:18 +0200

    Helpers for cleaning text

--- Analysis/clean_txt.py
+++ Analysis/clean_txt.py
@@ -1,154 +1,122 @@
 import re
 import sys
 import nltk
-#nltk.download("stopwords")
 from nltk.stem import WordNetLemmatizer
 
 sys.path.append('../..')
 sys.path.append('..')
 
 
-class CleanString:
+# FUNCTIONS OPERATING ON STRINGS #
 
-    def __init__(self, text, extra_stopwords):
-        self.text = text
-        self.extra_stopwords = extra_stopwords
+def to_lowercase(text):
+    """
+    Convert all characters in a string to lowercase
+    """
+    low_text = text.lower()
 
-    # FUNCTIONS OPERATING ON STRINGS #
+    return low_text
 
-    def to_lowercase(self, text=None):
-        """
-        Convert all characters in a string to lowercase
-        """
 
-        if not text:
-            text = self.text
+def remove_references(text):
+    """
+    Function removes reference from the input text
 
-        low_text = text.lower()
+    !Assumes that text is already lowercase!
+    """
 
-        return low_text
+    # Match 'references' starting from the end of the text (assumes text is already lowercase)
+    idx = text.rfind('references')
 
-    def remove_references(self, text=None):
-        """
-        Function removes reference from the input text
+    # Catching no references texts
+    if (idx == -1):
+        return text
+    else:
+        return text[:idx]
 
-        !Assumes that text is already lowercase!
-        """
 
-        if not text:
-            text = self.text
+def remove_stopwords(text, extra_stopwords):
+    """
+    Remove stop words from list of tokenized words
 
-        # Match 'references' starting from the end of the text (assumes text is already lowercase)
-        idx = text.rfind('references')
+    extra_stopwords: list of context specific stopwords
 
-        # Catching no references texts
-        if (idx == -1):
-            return text
-        else:
-            return text[:idx]
+    ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't",'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
-    def tokenize_text(self, text=None):
+    """
 
-        if not text:
-            text = self.text
+    # List of stopwords
+    stopwords = nltk.corpus.stopwords.words('english') + extra_stopwords
 
-        # Transform documents: tokenize and make lower case
-        words = text.split(' ')
+    # List of words that should not be removed
+    not_stopwords = ['about', 'between', 'few', 'more', 'most', 'other', 'some']
 
-        return words
+    stopwords = [stop for stop in stopwords if stop not in not_stopwords]
 
-    # FUNCTIONS OPERATING ON VECTORIZED TEXTS #
+    # Remove stop words
+    clean_words = ' '.join([word for word in text.split(' ') if word not in stopwords])
 
-    def remove_nonunicode(self, words=[]):
+    return clean_words
 
-        if not words:
-            words = self.tokenize_text()
 
-        # Exclude non unicode characters
-        clean_words = [re.sub(r'\W+', '', word) for word in words]
+def tokenize_text(text):
 
-        return clean_words
+    # Transform documents: tokenize and make lower case
+    words = text.split(' ')
 
-    def remove_nummeric(self, words=[]):
+    return words
 
-        if not words:
-            words = self.tokenize_text()
 
-        # Exclude nummeric
-        clean_words = [re.sub(r'[0-9]+', '', word) for word in words]
+# FUNCTIONS OPERATING ON VECTORIZED TEXTS #
 
-        return clean_words
+def remove_nonunicode(words, stops=True):
+    """
+    Accepts list of words and removes unicode characters from them
+    """
 
-    def remove_stopwords(self, words=[], extra_stopwords=[]):
-        """
-        Remove stop words from list of tokenized words
+    # Exclude non unicode characters
+    if stops is False:
+        clean_words = [re.sub(r'[^a-zA-Z\d\.]', '', word) for word in words]
+    else:
+        clean_words = [re.sub(r'[^a-zA-Z\d]', '', word) for word in words]
 
-        extra_stopwords: list of context specific stopwords
+    return clean_words
 
-        ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't",'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
-        """
-        if not words:
-            words = self.tokenize_text()
+def remove_nummeric(words):
 
-        # List of stopwords
-        stopwords = nltk.corpus.stopwords.words('english') + extra_stopwords
+    # Exclude nummeric
+    clean_words = [re.sub(r'[0-9]+', '', word) for word in words]
 
-        # List of words that should not be removed
-        not_stopwords = ['about', 'between', 'few', 'more', 'most', 'other', 'some']
+    return clean_words
 
-        stopwords = [stop for stop in stopwords if stop not in not_stopwords]
 
-        # Remove stop words
-        clean_words = [word for word in words if (word not in stopwords)]
+def lemmatize_verbs(words):
+    """
+    Lemmatize verbs in list of tokenized words
+    """
 
-        return clean_words
+    lemmatizer = WordNetLemmatizer()
+    lemmas = []
+    for word in words:
+        lemma = lemmatizer.lemmatize(word, pos='v')
+        lemmas.append(lemma)
+    return lemmas
 
-    def stem_words(self, words=[]):
-        """
-        Stem words in list of tokenized words
-        """
 
-        if not words:
-            words = self.tokenize_text()
+def normalize(text, extra_stopwords, exclude_stops=True):
 
-        stemmer = LancasterStemmer()
-        stems = []
-        for word in words:
-            stem = stemmer.stem(word)
-            stems.append(stem)
-        return stems
+    words = to_lowercase(text)
+    words = remove_references(words)
+    words = remove_stopwords(words, extra_stopwords)
+    words = tokenize_text(words)
+    words = remove_nonunicode(words, exclude_stops)
+    words = remove_nummeric(words)
+    # Exclude empty
+    words = [word for word in words if word != '']
+    # Exclude single and two letter words
+    words = [word for word in words if len(word) > 3]
 
-    def lemmatize_verbs(self, words=[]):
-        """
-        Lemmatize verbs in list of tokenized words
-        """
+    words = lemmatize_verbs(words)
 
-        if not words:
-            words = self.tokenize_text()
-
-        lemmatizer = WordNetLemmatizer()
-        lemmas = []
-        for word in words:
-            lemma = lemmatizer.lemmatize(word, pos='v')
-            lemmas.append(lemma)
-        return lemmas
-
-    def normalize(self, text=None):
-        if not text:
-            text = self.text
-
-        words = self.to_lowercase(text)
-        words = self.remove_references(words)
-        words = self.tokenize_text(words)
-        words = self.remove_nonunicode(words)
-        words = self.remove_nummeric(words)
-        words = self.remove_stopwords(words, self.extra_stopwords)
-        # Exclude empty
-        words = [word for word in words if word != '']
-        # Exclude single and two letter words
-        words = [word for word in words if len(word) > 2]
-
-        words = self.lemmatize_verbs(words)
-
-        return words
+    return words
